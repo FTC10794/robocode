add316081fa8054d9f37cb69515695bea8905490
Natalie Leuenberger <nrleuenb@gmail.com>
2018-02-10 12:05:33 -0800

    stop value

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/HolonomicTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/HolonomicTeleop.java
@@ -96,7 +96,7 @@ public class HolonomicTeleop extends OpMode {
 
         robot.motorLift.setPower(Range.clip(gamepad2.left_stick_y, -1, 1));
         robot.servoLeftWheel.setPower(Range.clip(gamepad2.right_stick_y, -1, 1));
-        robot.servoRightWheel.setPower(-1 * Range.clip(gamepad2.right_stick_y, -1, 1));
+        robot.servoRightWheel.setPower(-1 * Range.clip(gamepad2.right_stick_y + 0.15, -1, 1));
 
         /**
          * Gamepad 1
